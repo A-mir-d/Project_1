82d36eb1262fdc9408186377953d2eecd6ec29f6
Amir <amirbsn27@gmail.com>
2025-07-01 21:35:22 +0330

    Add file
    Add drop-down_menu_opener.js

---
+++ js/drop-down_menu_opener.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector('.category-button');
+    const dropdown = document.querySelector('.category-dropdown');
+
+    dropdown.style.display = 'none';
+
+    button.addEventListener('click', function (e) {
+        e.stopPropagation();
+        if (dropdown.style.display === 'none') {
+            dropdown.style.display = 'block';
+        } else {
+            dropdown.style.display = 'none';
+        }
+    });
+
+    dropdown.addEventListener('click', function () {
+        dropdown.style.display = 'none';
+    });
+
+    document.addEventListener('click', function (e) {
+        if (!dropdown.contains(e.target) && !button.contains(e.target)) {
+            dropdown.style.display = 'none';
+        }
+    });
+});
