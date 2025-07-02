40f86b7733a25b20d1f03ea4fba5fa2da5f5d5b2
Amir <amirbsn27@gmail.com>
2025-07-02 22:31:27 +0330

    Refactor code
    Make the header and footer codes reusable

--- js/drop-down_menu_opener.js
+++ js/drop-down_menu_opener.js
@@ -1,25 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector('.category-button');
-    const dropdown = document.querySelector('.category-dropdown');
+const button = document.querySelector('.category-button');
+const dropdown = document.querySelector('.category-dropdown');
 
-    dropdown.style.display = 'none';
+dropdown.style.display = 'none';
 
-    button.addEventListener('click', function (e) {
-        e.stopPropagation();
-        if (dropdown.style.display === 'none') {
-            dropdown.style.display = 'block';
-        } else {
-            dropdown.style.display = 'none';
-        }
-    });
-
-    dropdown.addEventListener('click', function () {
+button.addEventListener('click', function (e) {
+    e.stopPropagation();
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block';
+    } else {
         dropdown.style.display = 'none';
-    });
+    }
+});
 
-    document.addEventListener('click', function (e) {
-        if (!dropdown.contains(e.target) && !button.contains(e.target)) {
-            dropdown.style.display = 'none';
-        }
-    });
+dropdown.addEventListener('click', function () {
+    dropdown.style.display = 'none';
+});
+
+document.addEventListener('click', function (e) {
+    if (!dropdown.contains(e.target) && !button.contains(e.target)) {
+        dropdown.style.display = 'none';
+    }
 });
---
+++ js/element-adder.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    const codesToLoad = ["my-header", "my-footer"];
+
+    codesToLoad.forEach(function (elementId) {
+        const filePath = "../" + elementId + ".html";
+
+        fetch(filePath)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (htmlContent) {
+                document.getElementById(elementId).innerHTML = htmlContent;
+                //header-scripts_start
+                if (elementId === "my-header") {
+                    const scriptPath = ["js/drop-down_menu_opener.js", "js/timer_counter.js"]
+                    scriptPath.forEach(function (path) {
+                        const headerScript = document.createElement("script");
+                        headerScript.src = path;
+                        document.body.appendChild(headerScript);
+                    });
+                }//header-scripts_end
+                //footer-scripts_start 
+                else if(elementId === "my-footer"){
+                    const footerScript = document.createElement("script");
+                    footerScript.src = "https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js";
+                    footerScript.onload = function(){
+                        AOS.init();
+                    };
+
+                    document.body.appendChild(footerScript);
+                }//footer-scripts_end
+            });
+    });
+};
